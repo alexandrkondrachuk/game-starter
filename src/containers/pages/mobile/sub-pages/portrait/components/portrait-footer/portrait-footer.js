07f0ea9e9873cfddc73c1022836acd594ecd60c7
Marchelo2012 <32093842+Marchelo2012@users.noreply.github.com>
2021-11-01 00:38:55 +0200

    Footer and header of mobile portrait

--- src/containers/pages/mobile/sub-pages/portrait/components/portrait-footer/portrait-footer.js
+++ src/containers/pages/mobile/sub-pages/portrait/components/portrait-footer/portrait-footer.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './portrait-footer.scss';
+import getSymbolFromCurrency from 'currency-symbol-map';
+import * as _ from 'lodash';
+import { useSelector } from 'react-redux';
+import config from '../../../../../../../config';
 
 const PortraitFooter = () => {
+
+    const minBet = useSelector((state) => (_.get(state, 'game.currency.minBet')))
+    const maxBet = useSelector((state) => (_.get(state, 'game.limits[5].limit')))
+    //const name = config.get('name', '').split(' ');
+    const currency = useSelector((state) => (_.get(state, 'game.player.currency')))
+    const currencySymbol = getSymbolFromCurrency(currency);
+    const roundId = useSelector((state) => (_.get(state, 'game.newRound.id')))
+
     return (
         <div className="PortraitFooter">
             <div className="Footer__Message">
                 Footer Message
             </div>
             <div className="Footer__RoundInfo">
-                Footer Round Info
+                <div className="Footer__RoundInfo__Limits">
+                    <span className="name">
+                        {'Roulette'}
+                    </span>
+                    &nbsp;
+                    <span className="limits">
+                        {currencySymbol}
+                        {minBet}
+                        { '-' }
+                        {maxBet}
+                    </span>
+                </div>
+                <div className="Footer__RoundInfo__Round">
+                    #
+                    {roundId}
+                </div>
             </div>
         </div>
     );
--- src/containers/pages/mobile/sub-pages/portrait/components/portrait-status-bar/portrait-status-bar.js
+++ src/containers/pages/mobile/sub-pages/portrait/components/portrait-status-bar/portrait-status-bar.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './portrait-status-bar.scss';
+import getSymbolFromCurrency from 'currency-symbol-map';
+import * as _ from 'lodash';
+import { useSelector } from 'react-redux';
+//import PlayerModel from '../../../../../../../models/player/player';
+//import RouletteRoundModel from '../../../../../../../models/roulette-round/roulette-round';
 
 const PortraitStatusBar = () => {
+    
+    const balance = useSelector((state) => (_.get(state, 'game.player.balance')))
+    const totalBet = useSelector((state) => (_.get(state, 'game.player.totalBetOfCurrentRound')))
+    const currency = useSelector((state) => (_.get(state, 'game.player.currency')))
+    const currencySymbol = getSymbolFromCurrency(currency);
+
     return (
         <div className="PortraitStatusBar">
-            <div className="Status__Balance">
-                Balance Status
+            <div className="PortraitStatusBar__Status__Balance">
+                <div className="Balance">
+                    <div className="title">Balance</div>
+                    <div className="value">
+                        {currencySymbol}
+                        {balance}
+                    </div>
+                </div>
+                <div className="Bet">
+                    <div className="title">Total Bet</div>
+                    <div className="value">
+                        {currencySymbol}
+                        {totalBet}
+                    </div>
+                </div>
             </div>
-            <div className="Status__Round">
+            <div className="PortraitStatusBar__Status__Round">
                 Round Status
             </div>
         </div>

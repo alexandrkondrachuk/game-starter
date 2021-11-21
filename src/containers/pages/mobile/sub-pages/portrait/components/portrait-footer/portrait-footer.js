e98d2760fd865ee2b373b630bd34c70dc542197c
Marchelo2012 <32093842+Marchelo2012@users.noreply.github.com>
2021-11-21 05:11:32 +0200

    (mobile|portrait) add winners to footer

--- src/containers/pages/mobile/sub-pages/portrait/components/portrait-footer/portrait-footer.js
+++ src/containers/pages/mobile/sub-pages/portrait/components/portrait-footer/portrait-footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './portrait-footer.scss';
 import getSymbolFromCurrency from 'currency-symbol-map';
 import * as _ from 'lodash';
@@ -15,10 +15,36 @@ const PortraitFooter = () => {
     const roundId = useSelector((state) => (_.get(state, 'game.roundState.id')))
     const winners = useSelector((state) => (_.get(state, 'game.winners')))
 
+    const [footerMessage, setFooterMessage] = useState(<br/>)
+    const [isInitialRender, setIsInitialRender] = useState(true)
+
+    React.useEffect(() => {
+        if (Array.isArray(winners?.winners) && winners.winners.length > 0) {
+            setIsInitialRender(false);
+            let i = 1; 
+            console.log(winners);  
+            setFooterMessage(
+                <div>
+                    <span className="winner">{winners.winnersCount + " players wins "}</span>
+                    <span className="winnerSum">{winners.allPlayersWinsSum + " " + currencySymbol}</span>
+                </div>
+            );     
+            winners.winners.forEach((winner) => {
+                setTimeout(() => {
+                    setFooterMessage(<span className="winner">{currencySymbol + " " + winner.win + " " + winner.nickname}</span>);
+                }, i * 1500);
+                i++;
+            });  
+            setTimeout(() => {
+                setFooterMessage(<br/>);
+            }, i * 1500);
+        }
+    }, [winners]);
+
     return (
         <div className="PortraitFooter">
             <div className="Footer__Message">
-                <br/>
+                {footerMessage}
             </div>
             <div className="Footer__RoundInfo">
                 <div className="Footer__RoundInfo__Limits">

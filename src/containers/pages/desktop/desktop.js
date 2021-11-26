26797bdab54ccaad9e95697b83bd49ed3099320c
Marchelo2012 <32093842+Marchelo2012@users.noreply.github.com>
2021-11-26 22:22:09 +0200

    (desktop) add temporary text

--- src/containers/pages/desktop/desktop.js
+++ src/containers/pages/desktop/desktop.js
@@ -14,7 +14,16 @@ const Desktop = () => {
     return (
         <div className="Desktop">
             <CustomRedirect url={config.get('apiURLs.home')} isRedirectStatus={!readyState} />
-            <WebrtcPlayer/>
+            <div className="Desktop__Container">
+                <WebrtcPlayer/>
+                <div className="Text">
+                    <h1>Dear player</h1>
+                    <br/>
+                    <h2>The game is currently available on the mobile version.</h2>
+                    <h2>Very soon you will be able to enjoy the game in the desktop version.</h2>
+                    <h2>We are sure you will like it!</h2>
+                </div>
+            </div>
         </div>
     );
 };
